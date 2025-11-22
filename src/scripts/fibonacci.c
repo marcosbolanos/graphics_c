7c63a5df935b1729ecaa033ed1f42c3a10e66ccb
marcos <marcosqbv@gmail.com>
2025-11-23 00:29:46 +0100

    added fibonacci generator

---
+++ src/scripts/fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+  if (n == 1) {
+    return 0;
+  } else if (n == 2) {
+    return 1;
+  } else {
+    return (fibonacci(n - 1) + fibonacci(n - 2));
+  }
+}
+
+int main() {
+  int n_repeats = 10;
+  for (int i = 1; i < n_repeats; i++) {
+    int fibo = fibonacci(i);
+    printf("%d\n", fibo);
+  }
+}
